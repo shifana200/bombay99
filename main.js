9b45d927bcc88e9354a719778c11a7e56dd9d5dd
shifana200 <shifanasaleem50@gmail.com>
2024-09-12 16:41:11 +0530

    Bombay99

---
+++ main.js
@@ -0,0 +1,5 @@
+
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll', window.scrollY / (document.body.offsetHeight - window.innerHeight))
+//alert(window.scrollY)
+})
